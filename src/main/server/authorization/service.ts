9afc092d6b518e9e46da20a8418680c5815da7bf
Taxonomon <stronkge@proton.me>
2025-12-31 21:24:09 +0100

    bump

--- src/main/server/authorization/service.ts
+++ src/main/server/authorization/service.ts
@@ -1,38 +1,16 @@
-import {Player} from "../player/schema";
-import playerPrincipalsRepo from "./player-principal/repo";
-import logger from "../logging/logger";
-import playerUtils from "../player/utils";
-import {PlayerPrincipal} from "./player-principal/schema";
+import playerState from "../player/state";
 
-// TODO cache all perms and principals on join and don't go over database for each perm check anymore
-
-async function hasPermission(playerNetId: number, permission: string) {
-  const playerName = playerUtils.getPlayerNameFromNetId(playerNetId);
-
-  try {
-    const player: Player | undefined = await playerUtils.getPlayerFromNetId(playerNetId);
-
-    if (undefined === player) {
-      logger.warn(
-        `failed to check if player "${playerName}" (net id ${playerNetId}) has permission "${permission}": `
-        + `no player found for given net id`
-      );
-      return false;
-    }
-
-    const playerPrincipalsWithPermission: PlayerPrincipal[] =
-      await playerPrincipalsRepo.findByPlayerAndPermissionIdentifier(player, permission);
-
-    return playerPrincipalsWithPermission.length > 0;
-  } catch (error: any) {
-    logger.error(
-      `failed to check if player "${playerName}" (net id ${playerNetId}) has permission "${permission}": `
-      + `${error?.message.toString().trim().length > 0 ? error.message : error.stack}`
-    );
-    return false;
-  }
+async function hasPrincipal(netId: number, principal: string) {
+  return playerState.getConnectedPlayer(netId)?.principals.includes(principal);
 }
 
-const authorizationService = { hasPermission };
+async function hasPermission(netId: number, permission: string) {
+  return playerState.getConnectedPlayer(netId)?.permissions.includes(permission);
+}
+
+const authorizationService = {
+  hasPrincipal,
+  hasPermission
+};
 
 export default authorizationService;
